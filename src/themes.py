fa0400b15eb102cdeafe54624e130a342a2a1130
Brendan Desmond <brendes@gmail.com>
2025-02-04 23:05:34 -0500

    clean up and refine

--- src/themes.py
+++ src/themes.py
@@ -5,33 +5,37 @@ themes = {
         "theme_name": "Scotopic",
         "base_0": "#000000",
         "base_1": "#281818",
-        "base_x": "#482828",
-        "base_2": "#683838",
-        "base_3": "#cc6666",
-        "base_4": "#ee8888",
-        "base_5": "#ff9999",
+        "base_2": "#482828",
+        "base_3": "#683838",
+        "base_4": "#cc6666",
+        "base_5": "#ee8888",
+        "base_6": "#ff9999",
     },
     "deep": {
         "theme_name": "Scotopic Deep",
         "base_0": "#000000",
         "base_1": "#220000",
-        "base_2": "#581111",
-        "base_3": "#cc3333",
-        "base_4": "#ee4444",
-        "base_5": "#ff5555",
+        "base_2": "#441111",
+        "base_3": "#581111",
+        "base_4": "#cc3333",
+        "base_5": "#ee4444",
+        "base_6": "#ff5555",
     },
 }
 
 for key, theme in themes.items():
     theme["bg_main"] = theme["base_0"]
     theme["bg_subtle"] = theme["base_1"]
-    theme["bg_fade"] = theme["base_2"] + "88"
-    theme["bg_hl"] = theme["base_2"]
-    theme["bg_match"] = theme["base_3"] + "78"
+    theme["bg_faint"] = theme["base_2"]
+    # theme["bg_fade"] = theme["base_3"] + "88"
+    theme["bg_fade"] = theme["base_2"]
+    theme["bg_hl"] = theme["base_3"]
+    theme["bg_sel"] = theme["base_3"] + "e8"
+    theme["bg_match"] = theme["base_4"] + "78"
     theme["bg_drop"] = theme["bg_hl"] + "88"
-    theme["fg_bold"] = theme["base_5"]
-    theme["fg_main"] = theme["base_4"]
-    theme["fg_accent"] = theme["base_3"]
+    theme["fg_bold"] = theme["base_6"]
+    theme["fg_main"] = theme["base_5"]
+    theme["fg_accent"] = theme["base_4"]
     theme["fg_subtle"] = theme["fg_main"] + "c8"
     theme["fg_dim"] = theme["fg_main"] + "c0"
     theme["fg_fade"] = theme["fg_main"] + "88"
